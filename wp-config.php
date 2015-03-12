69954e171a29ee7a61072a7b59ff806d572036ee
Stan Wiechers <whoisstan@gmail.com>
2015-03-12 11:01:47 -0400

    debug

--- wp-config.php
+++ wp-config.php
@@ -14,8 +14,8 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
   /** MySQL database password */
   define('DB_PASSWORD', $_SERVER["RDS_PASSWORD"]);
   /** MySQL hostname */
-  define('DB_HOST', $_SERVER["RDS_HOSTNAME"]);  
-	
+  define('DB_HOST', $_SERVER["RDS_HOSTNAME"]);
+
 }
 
 // ========================
@@ -58,8 +58,8 @@ define( 'WPLANG', '' );
 // ===========
 // Hide errors
 // ===========
-ini_set( 'display_errors', 0 );
-define( 'WP_DEBUG_DISPLAY', false );
+ini_set( 'display_errors', 1 );
+define( 'WP_DEBUG_DISPLAY', true );
 
 // =================================================================
 // Debug mode
